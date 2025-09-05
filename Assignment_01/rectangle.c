50061f424ad9c72fbe5e20b92bba732788684f70
Sunil Kumar Sahni <errorslove2@gmail.com>
2025-09-06 03:12:42 +0530

    rectangle

---
+++ Assignment_01/rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int l, w , a;
+    printf("Enter the lenthr: ");
+    scanf("%d %d",&l);
+     printf("Enter the width: ");
+    scanf("%d %d",&w);
+    a = l*w;
+    printf("Are of rectangle = %d ",a);
+    return 0;
+}
